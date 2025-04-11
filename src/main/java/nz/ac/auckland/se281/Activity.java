a9dbb3de1114472c3d702c245120e1226b9d5c15
Sam Richell-Smith <samrs6012@gmail.com>
2025-04-11 13:00:06 +1200

    Implement average rating calculator into Activity class

--- src/main/java/nz/ac/auckland/se281/Activity.java
+++ src/main/java/nz/ac/auckland/se281/Activity.java
@@ -2,6 +2,7 @@ package nz.ac.auckland.se281;
 
 import java.util.ArrayList;
 import nz.ac.auckland.se281.Types.ActivityType;
+import nz.ac.auckland.se281.Types.ReviewType;
 
 public class Activity {
 
@@ -10,6 +11,7 @@ public class Activity {
   private Operator parentOp;
   private String id;
   private ArrayList<Review> reviews;
+  private double rating;
 
   public Activity(ActivityType type, String name, Operator parentOp, String id) {
     this.type = type;
@@ -37,6 +39,28 @@ public class Activity {
 
   public void addReview(Review review) {
     reviews.add(review);
+
+    // sets the new average rating
+    rating = calcRating();
+  }
+
+  public double getRating() {
+    return rating;
+  }
+
+  private double calcRating() {
+    // finds the average rating of activities
+
+    double totalRating = 0;
+    int rCount = 0;
+    for (Review r : reviews) {
+      if (r.getReviewType() != ReviewType.PRIVATE) {
+        totalRating += r.getRating();
+        rCount += 1;
+      }
+    }
+
+    return (totalRating / rCount);
   }
 
   public ArrayList<Review> getReviews() {
